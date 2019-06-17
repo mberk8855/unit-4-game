4152a8f23ca69a314a0d68a7438d721ebfa2f454
Meridith Berk <mberk8855@gmail.com>
2019-06-17 10:05:58 -0700

    new HW

---
+++ assets/javascript/game.js
@@ -0,0 +1,93 @@
+var ComputerNumber = Math.floor(Math.random() * 100) + 20;
+console.log("computer: ", ComputerNumber)
+//create a start button 
+
+$("#computer").text("Computer: " + ComputerNumber)
+//random numbers for crystals 
+
+
+var crystals = {
+
+    "blue": Math.floor(Math.random() * 12) + 1,
+    "green": Math.floor(Math.random() * 12) + 1,
+    "orange": Math.floor(Math.random() * 12) + 1,
+    "red": Math.floor(Math.random() * 12) + 1
+}
+
+console.log("crystals: ", crystals)
+
+var winsNumber = 0;
+var lossesNumber = 0;
+
+$("#wins").text("Wins: " + winsNumber)
+
+$("#losses").text("Losses: " + lossesNumber)
+var sum = 0;
+$("#sum").text("Sum: " + sum)
+
+
+
+
+$(".crystal").on("click", function (event) {
+
+    console.log("onclick")
+    console.log(this)
+    var crystalClicked = $(this).attr("id")
+    console.log(crystalClicked)
+
+    var value = crystals[crystalClicked]
+    console.log("value: ", value)
+
+    sum = sum + value;
+    $("#sum").text("Sum: " + sum)
+
+    if (sum > ComputerNumber) {
+        lossesNumber++
+        $("#losses").text("Losses: " + lossesNumber)
+        restart()
+    }
+    else if (sum === ComputerNumber) {
+        winsNumber++
+        $("#wins").text("Wins: " + winsNumber)
+        restart()
+    }
+
+
+})
+
+function restart(){
+    ComputerNumber = Math.floor(Math.random() * 100) + 20;
+    console.log("computer: ", ComputerNumber)
+    //create a start button 
+    
+    $("#computer").text("Computer: " + ComputerNumber)
+    //random numbers for crystals 
+    
+    
+    crystals = {
+    
+        "blue": Math.floor(Math.random() * 12) + 1,
+        "green": Math.floor(Math.random() * 12) + 1,
+        "orange": Math.floor(Math.random() * 12) + 1,
+        "red": Math.floor(Math.random() * 12) + 1
+    }
+    
+    console.log("crystals: ", crystals)
+    
+ 
+    sum = 0;
+    $("#sum").text("Sum: " + sum)
+    
+
+}
+
+
+
+
+//create a start button
+//clean up code 
+//send a message after loss and restart (do you want to play again?)
+//when on click of button then code to restart 
+//message saying you lost you win do you want to play again? 
+
+//then work on style 
